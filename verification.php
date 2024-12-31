30c74e490c5cb5e37d128a1c2b19f982b8459199
abbaslvk <abbasahmedovslvk@gmail.com>
2024-12-31 13:12:04 +0400

    Update verification.php

--- verification.php
+++ verification.php
@@ -1,7 +1,7 @@
 <?php
 
 if(isset($_GET["optoken"])){
-    $conn = new mysqli("sql306.infinityfree.com","if0_38006473","eKV6FOw7dtXlQFY","if0_38006473_database");
+    $conn = new mysqli("");
     if($conn->connect_error){
           http_response_code(500);
          echo "error 500: database related error.";
